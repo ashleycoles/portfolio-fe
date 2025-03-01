41fbd0fa1858180defb2d7af4fb4fe6b86509114
Ashley Coles <ashley@acoles.co.uk>
2025-03-01 09:09:40 +0000

    Handling post not found

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,8 +1,14 @@
 import { BASE_URL } from "$lib/settings/api";
 import type { Post } from "$lib/types/post.js";
+import { error } from "@sveltejs/kit";
 
 export async function load({ params }) {
     const res = await fetch(`${BASE_URL}/posts/${params.slug}`);
+
+    if (res.status === 404) {
+        error(404);
+    }
+
     const data: { data: Post } = await res.json();
 
     const post = data.data;
