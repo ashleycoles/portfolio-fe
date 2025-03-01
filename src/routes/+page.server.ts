819351343d16f498c6ad4d21c4f4afb0552fe93c
Ashley Coles <ashley@acoles.co.uk>
2025-03-01 08:28:33 +0000

    API base url const

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,9 @@
+import { BASE_URL } from '$lib/settings/api';
 import type { PostExcerpt } from '$lib/types/post';
 
 export async function load() {
-    const res = await fetch('http://localhost/api/posts');
+    
+    const res = await fetch(`${BASE_URL}/posts`);
     const data: { data: PostExcerpt[] } = await res.json();
 
     const posts: PostExcerpt[] = data.data
